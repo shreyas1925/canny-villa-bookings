536352df52d5a7f758033639b5b94dc1c0cdd803
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-09-28 22:20:24 +0530

    feat : adding home banner

--- Components/Banner.js
+++ Components/Banner.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import headerStyles from "../styles/Banner.module.css";
 
 const Banner = () => {
   return (
@@ -6,14 +7,19 @@ const Banner = () => {
       className="relative h-[310px] sm:h-[400px] md:h-[510px] xl:h-[710px] 2xl:h-[700
     px]"
     >
-      <Image
-        src="https://links.papareact.com/0fm"
-        layout="fill"
-        objectFit="cover"
-      />
+      <div className={headerStyles.image}>
+        {/* <Image
+          // src="https://links.papareact.com/0fm"
+          src={bannerimg}
+          layout="fill"
+          objectFit="cover"
+        /> */}
+      </div>
 
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go ?</p>
+        <p className="text-sm sm:text-lg text-bold text-white">
+          Not sure where to go ?
+        </p>
         <button
           className="bg-white text-purple-500 px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl
         active:scale-90 transition duration-150"
